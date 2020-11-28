cf2677fff7944b6ae5c1a11c6ae1fd8e7865c5b6
mollymoog <mmkover@gmail.com>
2020-11-27 22:47:40 -0500

    commit tree c6ea24f3f048d518ba40c16c093a50754eda41cb

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
+
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +14,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToys(name, imageUrl, likes) {
+  fetch("http://localhost:3000/toys", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name, imageUrl, likes})
+  })
+  .then(response => response.json())
+  .then(object => object.message.forEach(obj => toyCard(obj)))
+}
